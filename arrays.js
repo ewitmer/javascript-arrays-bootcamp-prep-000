66035fb8fae7c7bb767cdb8384fe29dd0de343ae
Erin Witmer <ecwitmer@gmail.com>
2017-12-06 03:49:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  var sameArray = array.unshift(element)
+  return sameArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
