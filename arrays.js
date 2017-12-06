95c2a7d3ee899ae37e311f24a0f9f4685ae618f6
Erin Witmer <ecwitmer@gmail.com>
2017-12-06 03:47:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,5 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var newArray = [element, ...array]
-  return newArray;
+  return [element, ...array]
 }
